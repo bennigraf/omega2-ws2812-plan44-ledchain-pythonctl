b4360ed9c18e269eab48d72ed65cc71a427d3613
Benjamin Graf <bennigraf@gmail.com>
2019-03-03 21:03:23 +0100

    Create flatten.py

---
+++ flatten.py
@@ -0,0 +1,15 @@
+# from http://rightfootin.blogspot.com/2006/09/more-on-python-flatten.html
+def flatten(l, ltypes=(list, tuple)):
+    ltype = type(l)
+    l = list(l)
+    i = 0
+    while i < len(l):
+        while isinstance(l[i], ltypes):
+            if not l[i]:
+                l.pop(i)
+                i -= 1
+                break
+            else:
+                l[i:i + 1] = l[i]
+        i += 1
+    return ltype(l)
